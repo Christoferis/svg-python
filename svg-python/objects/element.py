b02439a584f4740e1be3d34ea3176beece0ae9d2
Mikey Christoferis Qu4rkz <71943288+Christoferis@users.noreply.github.com>
2021-12-11 22:56:42 +0100

    split base class into base and visual class for better specification

--- svg-python/objects/element.py
+++ svg-python/objects/element.py
@@ -1,9 +1,10 @@
 #base element for all svg objects
 
 from typing import Union
+import util
 
-
-class element:
+# an abstract base class for concepts in svg (use for groups, visual elements like rects etc.)
+class base:
     _count = int()
 
     def __init__(self, tag : str, pos_x=0, pos_y=0, rot=0, w=10, h=10, id : str = str(_count)) -> None:
@@ -19,7 +20,7 @@ class element:
 
         #add to main buffer
         
-        element._count += 1
+        base._count += 1
         pass
 
     '''
@@ -30,7 +31,7 @@ class element:
         return (self.properties.get("x"), self.properties.get("y"), self.properties.get("rot"))
 
     def set_rotation(self, rotation : Union[int, float]):
-        self.properties.get("rot") = rotation
+        self.properties["rot"] = rotation
 
     '''
     change x and y position of the element, set a parameter None if it shouldn't be changed 
@@ -44,20 +45,34 @@ class element:
     '''
     Adds or update a given property
     '''
-    def add_update_property(self, property : Union[str, tuple], value=None):
+    def set_property(self, property : Union[str, tuple], value=None):
         self.properties[property] = value
         pass
+
+    def get_property(self, property):
+        return self.properties[property]
 
     '''
     removes and or returns value of given property -> raises KeyError if property doesn't exist
     '''
-    def remove_pop_property(self, property : str):
+    def remove_property(self, property : str):
         return self.properties.pop(property)
 
+    '''
+    a function for directly setting x, y and z
+    '''
+    def set_position_rotation(self, x : int = None, y : int = None, rot : int = None):
+        if x != None:
+            self.set_property("x", x)
+
+        if y != None:
+            self.set_property("y", y)
+
+        if rot != None:
+            self.set_property("rot", rot)
 
     def _get_id(self):
         return self.properties.get("id")
-
 
     '''
     (internal) generates a tag to give out to parser
@@ -69,3 +84,23 @@ class element:
             tag += '"' + key + '"' + "=" + '"' + str(self.properties.get()) + '"' + " "
 
         return tag + "/>"
+
+
+#a base class for all visual elements
+class visual(base):
+
+    def __init__(self, tag: str, pos_x=0, pos_y=0, rot=0, w=10, h=10, id: str = str(_count)) -> None:
+        super().__init__(tag, pos_x=pos_x, pos_y=pos_y, rot=rot, w=w, h=h, id=id)
+
+        self.properties["fill-color"] = util.color.RED
+        self.properties["opacity"] = 100
+        self.properties["stroke-color"] = util.color.BLUE
+        self.properties["stroke-width"] = 1
+
+    def set_fill(self, color : str):
+        self.set_property("fill-color", color)
+        pass
+
+    def set_opacity(self, alpha : int):
+        self.set_property("opacity", alpha)
+        pass
